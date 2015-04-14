8a72fcb5bee72e51d2ad45b5fce07ec02c088bc0
Vincent Fiorentini <vincent@panatechcomputer.com>
2015-04-14 13:49:35 -0700

    Initial Commit

---
+++ FinalProjectPanorama/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProjectPanorama
+//
+//  Created by Vincent Fiorentini on 4/14/15.
+//  Copyright (c) 2015 Vincent Fiorentini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
